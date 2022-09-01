25ce4cc132f75e6d258e454a747c095c103187e5
Julaxe <julaxe8@gmail.com>
2022-09-01 17:43:27 -0400

    navmesh created, input tested

--- MobaArena/Assets/_Scripts/Units/Character/CharacterMovement.cs
+++ MobaArena/Assets/_Scripts/Units/Character/CharacterMovement.cs
@@ -1,9 +1,38 @@
+using System;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem;
 
 namespace _Scripts.Units.Character
 {
     public class CharacterMovement : MonoBehaviour
     {
+        private NavMeshAgent _navMeshAgent;
+
         public bool isWalking;
+        
+        private void OnValidate()
+        {
+            _navMeshAgent = GetComponent<NavMeshAgent>();
+        }
+
+        private void OnRightClick(InputValue value)
+        {
+            //move to the position
+
+            var mousePos = Mouse.current.position;
+
+            Ray movePosition = Camera.main.ScreenPointToRay(mousePos.ReadValue());
+
+            if (Physics.Raycast(movePosition, out var hitInfo))
+            {
+                _navMeshAgent.SetDestination(hitInfo.point);
+            }
+        }
+
+        private void Update()
+        {
+            isWalking = _navMeshAgent.hasPath;
+        }
     }
 }
