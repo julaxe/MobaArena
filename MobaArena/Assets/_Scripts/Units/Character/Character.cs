8228cfd11a77fe5cc3008a478e22ec4bbcff9452
Julaxe <julaxe8@gmail.com>
2022-09-01 16:49:41 -0400

    Dummy created, new input system added

--- MobaArena/Assets/_Scripts/Units/Character/Character.cs
+++ MobaArena/Assets/_Scripts/Units/Character/Character.cs
@@ -1,29 +1,31 @@
 using _Scripts.Scriptables;
 using UnityEngine;
 
-public class Character : MonoBehaviour
+namespace _Scripts.Units.Character
 {
-    [SerializeField] private ScriptableCharacterBase baseCharacter;
-    
-    //spells
-    
-    private void Awake() => ExampleGameManager.OnBeforeStateChanged += OnStateChanged;
-
-    private void OnDestroy() => ExampleGameManager.OnBeforeStateChanged -= OnStateChanged;
-
-    private void OnStateChanged(GameState newState)
+    public class Character : MonoBehaviour
     {
-        
-    }
-
-    private void OnMouseDown() {
-        // Only allow interaction when it's the hero turn
-        if (ExampleGameManager.Instance.State != GameState.HeroTurn) return;
-
-        // Eventually either deselect or ExecuteMove(). You could split ExecuteMove into multiple functions
-        // like Move() / Attack() / Dance()
-
-        Debug.Log("Unit clicked");
-    }
+        [SerializeField] private ScriptableCharacterBase baseCharacter;
     
+        //spells
+    
+        private void Awake() => ExampleGameManager.OnBeforeStateChanged += OnStateChanged;
+
+        private void OnDestroy() => ExampleGameManager.OnBeforeStateChanged -= OnStateChanged;
+
+        private void OnStateChanged(GameState newState)
+        {
+        
+        }
+
+        private void OnMouseDown() {
+            // Only allow interaction when it's the hero turn
+
+            // Eventually either deselect or ExecuteMove(). You could split ExecuteMove into multiple functions
+            // like Move() / Attack() / Dance()
+
+            Debug.Log("Unit clicked");
+        }
+    
+    }
 }
