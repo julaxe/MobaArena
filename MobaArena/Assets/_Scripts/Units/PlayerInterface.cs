3e698543f7136d922293a0db29caf5f4957a86e0
Julaxe <julaxe8@gmail.com>
2022-09-23 18:26:42 -0400

    Cooldown for the 4 spells are working

--- MobaArena/Assets/_Scripts/Units/PlayerInterface.cs
+++ MobaArena/Assets/_Scripts/Units/PlayerInterface.cs
@@ -10,8 +10,17 @@ namespace _Scripts.Units
     public class PlayerInterface : MonoBehaviour
     {
         [SerializeField] private UIDocument _uiDocument;
-        [SerializeField] private Image image;
+        [SerializeField] private Image imageSpell1;
+        [SerializeField] private Image imageSpell2;
+        [SerializeField] private Image imageSpell3;
+        [SerializeField] private Image imageSpell4;
         [SerializeField] private float cooldown;
+
+        public bool IsSpell1Available() => !imageSpell1.enabled;
+        public bool IsSpell2Available() => !imageSpell2.enabled;
+        public bool IsSpell3Available() => !imageSpell3.enabled;
+        public bool IsSpell4Available() => !imageSpell4.enabled;
+        
         
 
         private VisualElement root;
@@ -19,20 +28,35 @@ namespace _Scripts.Units
         private void Start()
         {
             root = _uiDocument.rootVisualElement;
-            root.Q<Button>("Q").clicked += SetAbilityOnCooldown;
+            root.Q<Button>("Q").clicked += () => SetAbilityOnCooldown(imageSpell1);
+            root.Q<Button>("W").clicked += () => SetAbilityOnCooldown(imageSpell2);
+            root.Q<Button>("E").clicked += () => SetAbilityOnCooldown(imageSpell3);
+            root.Q<Button>("R").clicked += () => SetAbilityOnCooldown(imageSpell4);
         }
 
-        private void SetAbilityOnCooldown()
+        private void SetAbilityOnCooldown(Image imageSpell)
         {
-            StartCoroutine(CoolDownCoroutine(cooldown));
+            StartCoroutine(CoolDownCoroutine(cooldown, imageSpell));
         }
 
         private void OnSpell1(InputValue value)
         {
-            SetAbilityOnCooldown();
+            SetAbilityOnCooldown(imageSpell1);
+        }
+        private void OnSpell2(InputValue value)
+        {
+            SetAbilityOnCooldown(imageSpell2);
+        }
+        private void OnSpell3(InputValue value)
+        {
+            SetAbilityOnCooldown(imageSpell3);
+        }
+        private void OnSpell4(InputValue value)
+        {
+            SetAbilityOnCooldown(imageSpell4);
         }
 
-        private IEnumerator CoolDownCoroutine(float cd)
+        private IEnumerator CoolDownCoroutine(float cd, Image image)
         {
             if (image.enabled) yield break;
             float startTime = Time.time;
